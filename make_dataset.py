9a33ba57200996a212adf4caa64d94bed8d69b5d
jungwon_moon <jwoung0@naver.com>
2021-04-30 06:21:43 +0900

    add except

--- make_dataset.py
+++ make_dataset.py
@@ -28,21 +28,23 @@ def make_dataset(files, label="nomask"):
     count = 0   # 파일이름으로 사용
     
     for file in files:
+        try:
+            img = cv2.imread(file)
+            faces, conf = cv.detect_face(img, threshold=0.6)
 
-        img = cv2.imread(file)
-        faces, conf = cv.detect_face(img, threshold=0.6)
-
-        if faces == []:
-            continue
-        
-        (h, w) = img.shape[:2]
-        for face in faces:
-            
-            if face[2] > w or face[3] > h:  # bbox가 잘못 지정된 경우 제외
+            if faces == []:
                 continue
-            count += 1
-            face_img = img[face[1]:face[3],face[0]:face[2]]
-            cv2.imwrite(f"dataset/{label}/{count:04}.jpg", face_img)
+            
+            (h, w) = img.shape[:2]
+            for face in faces:
+                
+                if face[2] > w or face[3] > h:  # bbox가 잘못 지정된 경우 제외
+                    continue
+                count += 1
+                face_img = img[face[1]:face[3],face[0]:face[2]]
+                cv2.imwrite(f"dataset/{label}/{count:04}.jpg", face_img)
+        except:
+            print(file)     # 에러 파일 확인
 
 
 make_dataset(mask_files, "mask")        # mask   레이블 생성
